94da475b8ceb4a0d8b6d8d1b996513f6b7ed1d88
Brian Snider <bsnider@georgefox.edu>
2022-01-04 09:48:56 -0800

    initial commit

---
+++ by_ref.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file by_ref.cpp
+ * @brief Implementation for the by_ref program.
+ *
+ * @author Brian R. Snider (bsnider@georgefox.edu)
+ */
+
+#include "by_ref.h"
+
+int main(int argc, char* argv[])
+{
+    // TODO implement program
+}
+
+// TODO implement helper methods
+
---
+++ by_ref.h
@@ -0,0 +1,11 @@
+/**
+ * @file by_ref.h
+ * @brief Declarations for the by_ref program.
+ *
+ * @author Brian R. Snider (bsnider@georgefox.edu)
+ */
+
+#pragma once
+
+// TODO declare helper methods
+
